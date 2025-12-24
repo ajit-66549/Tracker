ef97c720e03a8e421bbd61f48b86088b11f4738a
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-12-24 17:45:37 -0600

    Coorect syntax error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import './App.css'
+import { useState } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <>
-      isLoggedIn ? (
-        <Dashboard />
-      ) : (
-        <Login onLogin={() => setIsLoggedIn(true)} />
-      )
-    </>
+  return isLoggedIn ? (
+    <Dashboard />
+  ) : (
+    <Login onLogin={() => setIsLoggedIn(true)} />
   );
 };
 
